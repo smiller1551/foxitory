47995b8050fe55aa7924317464e952faf123e46e
Climax9 <simon325@terpmail.umd.edu>
2023-04-19 13:17:56 -0400

    moved stuff around, created map_init function

--- fox.py
+++ fox.py
@@ -2,11 +2,32 @@ import random
 import time
 
 class Fox:
+    """This class stores information about the fox.
+    """
 
     def __init__(self, location):
         self.location = location
+        
+class Location:
+    """This class stores information about locations.
+    """
+    
+    def __init__(self, name, hidingspots, present = False):
+        self.name = name
+        self.hidingspots = hidingspots
+        self.present = present
+        
+class HidingSpot:
+    """This class stores information about hiding spots.
+    """
+    
+    def __init__(self, name, present = False):
+        self.name = name
+        self.present = present
                 
 def foxtrot(active = False):
+    """This function creates a fox and moves it around.
+    """
     startlist = list(Map.keys()) #grab the keys of the Map dict. and turn them into a list
     startlocation = random.choice(startlist) #grab a random location from the list
     Foxtudo = Fox(startlocation) #create an instance of the Fox and assign it to the start location
@@ -26,26 +47,22 @@ def foxtrot(active = False):
             active = False #stop the fox
             print("The Fox is gone!") #TESTING: print that the fox is gone.
             
-class Location:
+def Map_init(Map):
+    """
+    This function initializes the map. It must be run before the game starts!
+    """
     
-    def __init__(self, name, hidingspots, present = False):
-        self.name = name
-        self.hidingspots = hidingspots
-        self.present = present
-        
-class HidingSpot:
     
-    def __init__(self, name, present = False):
-        self.name = name
-        self.present = present
-        
+  
 Map = {"Morill Quad": ("Chapel Gardens", "McKeldin Mall"),
-       "Chapel Gardens": ("Morill Quad", "McKeldin Mall"),
-       "McKeldin Mall": ("Morill Quad", "Chapel Gardens", "Engineering Fields", "Ampitheater"),
-       "Ampitheater": ("Yahentamitsi", "Regents Drive Garage", "McKeldin Mall"),
-       "Yahentamitsi": ("Ampitheater", "Regents Drive Garage"),
-       "Regents Drive Garage": ("Ampitheater", "Engineering Fields", "Paint Branch Trail", "Yahentamitsi"),
-       "Engineering Fields": ("McKeldin Mall", "Regents Drive Garage", "Paint Branch Trail"),
-       "Paint Branch Trail": ("Regents Drive Garage", "Engineering Fields", "Beyond")}
+    "Chapel Gardens": ("Morill Quad", "McKeldin Mall"),
+    "McKeldin Mall": ("Morill Quad", "Chapel Gardens", "Engineering Fields", "Ampitheater"),
+    "Ampitheater": ("Yahentamitsi", "Regents Drive Garage", "McKeldin Mall"),
+    "Yahentamitsi": ("Ampitheater", "Regents Drive Garage"),
+    "Regents Drive Garage": ("Ampitheater", "Engineering Fields", "Paint Branch Trail", "Yahentamitsi"),
+    "Engineering Fields": ("McKeldin Mall", "Regents Drive Garage", "Paint Branch Trail"),
+    "Paint Branch Trail": ("Regents Drive Garage", "Engineering Fields", "Beyond")}
+
+    
 
 foxtrot(True) #for testing purposes
