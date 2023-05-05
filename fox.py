9544c48029b4e851f722fa4485b59d7d512de4cb
Climax9 <simon325@terpmail.umd.edu>
2023-05-05 13:22:32 -0400

    fox now works!

--- fox.py
+++ fox.py
@@ -1,21 +1,43 @@
 import random
 from time import sleep
 import multiprocessing as mp
+from datetime import datetime, timedelta
+from re import sub
 
-Map = {"Morill_Quad": ("Chapel_Gardens", "McKeldin_Mall"),
-    "Chapel_Gardens": ("Morill_Quad", "McKeldin_Mall"),
-    "McKeldin_Mall": ("Morill_Quad", "Chapel_Gardens", "Engineering_Fields", "Amphitheater"),
-    "Amphitheater": ("Regents_Drive_Garage", "McKeldin_Mall"),
-    "Regents_Drive_Garage": ("Amphitheater", "Engineering_Fields", "Paint_Branch_Trail"),
-    "Engineering_Fields": ("McKeldin_Mall", "Regents_Drive_Garage", "Paint_Branch_Trail"),
-    "Paint_Branch_Trail": ("Regents_Drive Garage", "Engineering_Fields", "Beyond")}
+Map = {"morill_quad": ("chapel_gardens", "mckeldin_mall"),
+    "chapel_gardens": ("morill_quad", "mckeldin_mall"),
+    "mckeldin_mall": ("morill_quad", "chapel_gardens", "engineering_fields", "amphitheater"),
+    "amphitheater": ("regents_drive_garage", "mckeldin_mall"),
+    "regents_drive_garage": ("amphitheater", "engineering_fields", "paint_branch_trail"),
+    "engineering_fields": ("mckeldin_mall", "regents_drive_garage", "paint_branch_trail"),
+    "paint_branch_trail": ("regents_drive_garage", "engineering_fields", "beyond")}
+
 
 class Fox:
     """This class stores information about the fox.
     """
 
-    def __init__(self, location):
+    def __init__(self, found = False, location = "Beyond"):
+        self.found = found
         self.location = location
+        
+    def return_str(self):
+        return str(self.location)
+        
+    def spawn(self):
+        
+        startlist = list(Map.keys()) #grab the keys of the Map dict. and turn them into a list
+        startlocation = random.choice(startlist) #grab a random location from the list
+        self.location = startlocation #create an instance of the Fox and assign it to the start location
+        
+        
+    def foxtrot(self):
+        
+        possibleloc = Map.get(self.location) #get a list of the nearest locations to the current locations from the map
+        nextlocation = random.choice(possibleloc) #grab a random location from the list
+        self.location = nextlocation #have the fox travel to that location
+        #print(self.location) #TESTING: print the fox location.
+        
         
 class Location:
     """This class stores information about locations.
@@ -26,63 +48,39 @@ class Location:
         self.present_prompt = present_prompt
         self.absent_prompt = absent_prompt
         self.present = present
-                
-def foxtrot(active = False):
-    """This function creates a fox and moves it around.
-    """
-    startlist = list(Map.keys()) #grab the keys of the Map dict. and turn them into a list
-    startlocation = random.choice(startlist) #grab a random location from the list
-    Foxtudo = Fox(startlocation) #create an instance of the Fox and assign it to the start location
         
-    #while the fox is active do the following...
-    while active == True:
-            
-        sleep(3) #have the fox sleep in the current location for a set amount of time
-        possibleloc = Map.get(Foxtudo.location) #get a list of the nearest locations to the current locations from the map
-        nextlocation = random.choice(possibleloc) #grab a random location from the list
-        Foxtudo = Fox(nextlocation) #have the fox travel to that location
-        print(Foxtudo.location) #TESTING: print the fox location.
-
-        #if the fox goes beyond the campus
-        if Foxtudo.location == "Beyond":
-            
-            active = False #stop the fox
-            print("The Fox is gone!") #TESTING: print that the fox is gone.
-            
 def Map_init():
     """
     This function initializes the map. It must be run before the game starts!
     """
-    locationlist = list(Map.keys())
     
-    Morill_Quad = ("Morill Quad", 
+    morill_quad = Location("Morill Quad",
                    "A photograph of a fox walking through a wooded area with low hanging trees and early 20th century brick university buildings.",
                    "A photograph of a wooded area with low hanging trees and early 20th century brick university buildings.")
-    Chapel_Gardens = ("Chapel Gardens",
+    chapel_gardens = Location("the Chapel Gardens",
                       "A photograph of a fox sleeping in a garden with red and black-eyed susan flowers.",
                       "A photograph of a wooded garden with an American chapel steeple rising from above the trees.")
-    McKeldin_Mall = ("McKeldin Mall", 
+    mckeldin_mall = Location("McKeldin Mall",
                      "A photograph of a fox walking across the University of Maryland's McKeldin Mall.", 
                      "A photograph of the University of Maryland's McKeldin Mall.")
-    Ampitheater = ("Amphitheater", 
-                   "A photograph of a fox in a grassy amphitheater made of brick."
+    ampitheater = Location("Nyumburu Amphitheater",
+                   "A photograph of a fox in a grassy amphitheater made of brick.",
                    "A photograph of an amphitheater in front of a brick building.")
-    Regents_Drive_Garage = ("Regents Drive Garage",
-                            "A photograph of a fox inside a parking garage that is full of cars."
+    regents_drive_garage = Location("Regents Drive Garage",
+                            "A photograph of a fox inside a parking garage that is full of cars.",
                             "A photograph from inside a parking garage that is full of cars")
-    Engineering_Fields = ("Engineering Fields",
-                          "A photograph of a fox running across a freshly cut grass field with dirt mounds and construction equipment in the distance."
+    engineering_fields = Location("the Engineering Fields",
+                          "A photograph of a fox running across a freshly cut grass field with dirt mounds and construction equipment in the distance.",
                           "A photograph a freshly cut grassy field with orange safety fence the backround.")
-    Paint_Branch_Trail = ("Paint Branch Trail",
-                          "A photograph looking down a paved wooded trail with a fox running away."
+    paint_branch_trail = Location("Paint Branch Trail",
+                          "A photograph looking down a paved wooded trail with a fox running away.",
                           "A photograph looking down a paved wooded trail.")
-     
-    
-#def locationcheck(location):
     
 def main():
     
-    print("Hey, Brandon! Welcome back to another great semester at the University of Maryland!")
+    Map_init() #create the locations
+    
+    print("Hey, B! Welcome back to another great semester at the University of Maryland!")
     sleep(2)
     print("I knew you were trying to get a photo of that sneaky fox last semester. I just overheard someone say it was on the run again!")
     sleep(3)
@@ -96,20 +94,78 @@ def main():
     sleep(1)
     print("     Ampitheater")
     sleep(1)
-    print("     Yahentamitsi Dining Hall")
-    sleep(1)
     print("     Regents Drive Garage")
     sleep(1)
     print("     Engineering Fields")
     sleep(1)
     print("     Paint Branch Trail")
     sleep(1)
+    print("Haha, I forgot to mention! You're blind and you can't see anything unless you take a picture!")
+    sleep(2)
+    print("The chip in your brain lets you see what your camera takes a picture of! Totally normall stuff and not game design limitations. :)")
+    sleep(3)
+    print("Although you can't see the fox, it can see you! The longer you stay in a location the quicker the fox will run away!")
+    print(" ")
     
-    location = input("Enter the location you want to visit: ")
+    points = 51
+    
+    Foxtudo = Fox() #create the fox
+    Foxtudo.spawn() #spawn the fox on the map
+    #print(Foxtudo.location)
+    
+    sleep(2)
+    
+    while Foxtudo.found == False or Foxtudo.location != "Beyond":
+        
+        sleep(1)
+        
+        first_time = datetime.now()
+        print(" ")
+        location_input = input("Enter the location you want to visit: ")
+        
+        location_format = (sub(r'\s', '_', location_input)).lower() #replace the spaces with underscores and make it lowercase
+        location_upper = location_input.upper()
+        
+        sleep(1)
+        
+        print(f"Welcome to {location_upper}!") #print where the person is at.
+        print(" ")
+        response = input("Do you want to take a picture? Enter yes or no: ")
+        response = response.lower() #make the entire response lowercase
+        
+        second_time = datetime.now()
+        
+        if response == "yes":
+            
+            difference = second_time - first_time #find the difference between the first and second time.
+    
+            if difference > timedelta(seconds=20): #if the time is greater then 20 seconds...
+    
+                Foxtudo.foxtrot() #...move the fox
+                points -= 5 #subtract 5 points
+                print("greater than 20 seconds")
+        
+            if location_format == Foxtudo.return_str():
+                
+                Foxtudo = Fox(found = True)
+                print("Hey! You found the fox! What a shot!")
+                print(f"Total Score: {points} points")
+                break
+                #return print(location.present_prompt)
 
+            else:
+                
+                print("Wow! There's no fox in this picture. Better move on...")
+                points -= 1
+                #return print(location.absent_prompt)
+        
+        Foxtudo.foxtrot() #move the fox regardless of the time
+        
+    if Foxtudo.location == "Beyond!":
+        
+        print("Hey, B! Just checked reddit and people are saying the fox is in town. I think we missed it. :(")
+        print("     G A M E  O V E R")
+        
+        
 if __name__ == "__main__":
-    main_p = mp.Process(target=main)
-    fox_p = mp.Process(target=foxtrot,args=[True])
-    
-    main_p.start()
-    fox_p.start()
+    main()
