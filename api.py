87926c1698791d522fbd4853583ad9be969ffd53
jjllehning <jjllehning@gmail.com>
2023-05-04 20:51:22 -0400

    Update api.py
    made it a class, added __init__ and key attribute, a few empty asserts and returns the image_url now

--- api.py
+++ api.py
@@ -1,25 +1,38 @@
 import os
 import openai
 
+#"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"
 
+class Image:
 
-def createImage(user_input):
-    """Create an image through DALL-E via user prompt.
+    def __init__(self, key):
+        """Access to DALLE-E API via key.
+        """
+        self.key = key
+        openai.api_key = os.getenv(self.key)
+        
+    def createImage(user_input):
+        """Create an image through DALL-E via user prompt.
 
-        prompt (str): required, description of desired image, <1000 characters
-        n (int): amount of images desired
-        size (str): size of image
-    """
-    #openai.organization = "YOUR_ORG_ID"
-    openai.api_key = os.getenv("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")
+            response (Image): the AI generated image
+            prompt (str): required, description of desired image, <1000 characters
+            n (int): amount of images desired
+            size (str): size of image
+        """
     
-    response = openai.Image.create(
-    prompt= user_input,
-    n=1,
-    size="512x512"
-    )
+        response = openai.Image.create(
+        prompt= user_input,
+        n=1,
+        size="512x512"
+        )
     
-    image_url = response['data'][0]['url']
-    #pull image through GUI 
+        image_url = response['data'][0]['url']
+        return image_url
+        #pull image through GUI 
 
-createImage("a fox at the University of Maryland College Park McKeldin Mall")
+fox_E = Image("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")
+img = fox_E.createImage("a fox at the University of Maryland College Park McKeldin Mall")
+
+assert(fox_E)
+assert(fox_E.key == "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")
+assert(img)
