6ea54906a4eff348370b8a80f30d99bbc3767305
Climax9 <simon325@terpmail.umd.edu>
2023-05-16 16:37:26 -0400

    updated

--- fox.py
+++ fox.py
@@ -1,7 +1,6 @@
 import openai
 import random
 from time import sleep
-import multiprocessing as mp
 from datetime import datetime, timedelta
 from re import sub
 import openai
@@ -66,7 +65,7 @@ def createImage(user_input,key):
             n (int): amount of images desired
             size (str): size of image
         """
-        openai.api_key = os.getenv(key)
+        openai.api_key = key
         response = openai.Image.create(
         prompt= user_input,
         n=1,
@@ -83,7 +82,7 @@ def main():
     As long as the fox's found value is False and the location is not beyond, a loop will run where the player goes to locations and
     tries to take a picture.
     As long as the player enters yes when they are prompted to take a picture the following logic will occur.
-        If the time it takes for the player to insert the location and if they want to take a picture is greater then 20 seconds, the fox will 
+        If the time it takes for the player to insert the location and if they want to take a picture is greater then 30 seconds, the fox will 
         move before the player gets a chance to try and take a picture and 5 points will be subtracted.
         If the fox is at the chosen location, the fox's found value will be sent to true, there will be a call to the api with the 
         present_prompt and the game will end.
@@ -109,31 +108,38 @@ def main():
                           "A photograph looking down a paved wooded trail.")
     }
     
+    print("Before playing please visit the following website to recieve an API key. Create an account with a free trial if need be.")
+    print(" ")
+    print("https://platform.openai.com/account/api-keys")
+    print(" ")
+    api_key = input("Enter the API key from openai before playing : ")
+    print("=====================================")
+    sleep(2)
     
     print("Hey, B! Welcome back to another great semester at the University of Maryland!")
-    #sleep(2)
+    sleep(2)
     print("I knew you were trying to get a photo of that sneaky fox last semester. I just overheard someone say it was on the run again!")
-    #sleep(3)
+    sleep(3)
     print("Go grab your camera and find it! I know it likes to frequent the following locations:")
-    #sleep(2)
+    sleep(2)
     print("     Morill Quad")
-    #sleep(1)
+    sleep(1)
     print("     Chapel Gardens")
-    #sleep(1)
+    sleep(1)
     print("     McKeldin Mall")
-    #sleep(1)
+    sleep(1)
     print("     Ampitheater")
-    #sleep(1)
+    sleep(1)
     print("     Regents Drive Garage")
-    #sleep(1)
+    sleep(1)
     print("     Engineering Fields")
-    #sleep(1)
+    sleep(1)
     print("     Paint Branch Trail")
-    #sleep(1)
+    sleep(1)
     print("Haha, I forgot to mention! You're blind and you can't see anything unless you take a picture!")
-    #sleep(2)
+    sleep(2)
     print("The chip in your brain lets you see what your camera takes a picture of! Totally normall stuff and not game design limitations. :)")
-    #sleep(3)
+    sleep(3)
     print("Although you can't see the fox, it can see you! The longer you stay in a location the quicker the fox will run away!")
     print(" ")
     
@@ -141,11 +147,16 @@ def main():
     
     Foxtudo = Fox() #create the fox
     Foxtudo.spawn() #spawn the fox on the map
-    #print(Foxtudo.location)
     
     sleep(2)
     
-    while Foxtudo.found == False or Foxtudo.location != "beyond": #while the fox isn't found or beyond...
+    while Foxtudo.found == False: #while the fox isn't found or beyond...
+        
+        if Foxtudo.location == "beyond": #if the fox is at beyond...
+        
+            print("Hey, B! Just checked reddit and people are saying the fox is downtown. I think we missed it. :(")
+            print("     Total Score: 0 points")
+            print("     G A M E  O V E R")
         
         sleep(1)
         
@@ -154,38 +165,34 @@ def main():
         location_input = input("Enter the location you want to visit: ") #have the user enter yes or no
         location_format = (sub(r'\s', '_', location_input)).lower() #replace the spaces with underscores and make it lowercase
         
-        #NEED ERROR HANDLING
-        #needs to be one of the locations listed in the def Map_init() or the map dict. at the beginning
-        #line of code above will reaplce the space with underscores and make everything lowercase to make matching easier
-        
+        sleep(1)
+        print(" ")
+        print("=====================================")
+        print(f"Welcome to {location_input.upper()}!") #print where the person is at.
+        print("=====================================")
+        print(" ")
         sleep(1)
         
-        print(f"Welcome to {location_input.upper()}!") #print where the person is at.
-        print(" ")
         response = input("Do you want to take a picture? Enter yes or no: ") #have the user enter yes or no
         response = response.lower() #make the entire response lowercase
-        
-        #NEED ERROR HANDLING    
-        #needs to be either yes or no, case does not matter
-        #case does not matter, because the line of code above will make it lower
         
         second_time = datetime.now() #grab the time
         
         if response == "yes":
-            api_key = input("Enter the API key from openai before playing : ")
+
             difference = second_time - first_time #find the difference between the first and second time.
     
-            if difference > timedelta(seconds=20): #if the time is greater then 20 seconds...
+            if difference > timedelta(seconds=30): #if the time is greater then 20 seconds...
     
                 Foxtudo.foxtrot() #...move the fox
                 points -= 5 #subtract 5 points
-                print("greater than 20 seconds") #for testing
+                print("greater than 30 seconds") #for testing
         
             if location_format == str(Foxtudo.location): #if the fox is at the location...
                 
                 Foxtudo = Fox(found = True) #set the fox to found
                 
-                createImage(prompt[location_format][0],api_key)
+                createImage(Prompts[location_format][0],api_key)
                 
                 print("Hey! You found the fox! What a shot!")
                 print(f"    Total Score: {points} points")
@@ -194,20 +201,13 @@ def main():
             else: #if the fox isn't at the location
                 
                 
-                createImage(prompt[location_format][1],api_key)
+                createImage(Prompts[location_format][1],api_key)
                 
                 print("Wow! There's no fox in this picture. Better move on...")
                 points -= 1
                 #return print(location.absent_prompt)
         
         Foxtudo.foxtrot() #move the fox regardless of the time
-        
-    if Foxtudo.location == "beyond": #if the fox is at beyond...
-        
-        print("Hey, B! Just checked reddit and people are saying the fox is downtown. I think we missed it. :(")
-        print("     Total Score: 0 points")
-        print("     G A M E  O V E R")
-        
-        
+         
 if __name__ == "__main__":
     main()
